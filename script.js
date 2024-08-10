71657112a9944d6ee4dab748828e130959d7ae0e
Adonis <me@zekkie.dev>
2024-08-10 19:35:28 +0300

    Fallback implemented if autoplay fails

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ window.addEventListener('load', () => {
     const video = document.getElementById('background-video');
     const content = document.getElementById('content');
 
+    video.addEventListener('canplay', () => {
+        video.play();
+    });
+
     video.addEventListener('ended', () => {
         content.style.visibility = 'visible';
         video.style.display = 'none';
     });
+
+    setTimeout(() => {
+        if (video.paused || video.ended) {
+            content.style.visibility = 'visible';
+            video.style.display = 'none';
+        }
+    }, 5000);
 });
